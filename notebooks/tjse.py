2b3545bd98077648d1de9ef1faf99ac48731dc3f
ranophoenix <ranomail@gmail.com>
2018-02-10 06:26:39 -0300

    Gráficos

--- notebooks/tjse.py
+++ notebooks/tjse.py
@@ -22,3 +22,36 @@ def carregar_dados_experimento1():
     corpora['Stemmer'] = corpora['Stemmer'].apply(mudar_rslps)
     
     return corpora
+
+def carregar_dados_experimento2():
+    colecoes = {
+                'asg':'ASG',
+                'dsg':'DSG',
+                'atr':'ATR',
+                'dtr': 'DTR'
+               }
+    algoritmos = {
+                    'nostem':'NoStem', 
+                    'porter' : 'Porter', 
+                    'rslp' : 'RSLP',
+                    'rslps' : 'RSLP-S', 
+                    'unine' : 'UniNE'
+                }
+    metricas = {
+            'map' : 'MAP', 
+            'precR' : 'MPR', 
+            'p10' : 'MPC'
+            }
+    
+    dfs = []
+    
+    for colecao_chave, colecao_alias in colecoes.items():
+        for algoritmo_chave, algoritmo_alias in algoritmos.items():
+            for metrica_chave, metrica_alias in metricas.items():
+                df = pd.read_csv('../csv/' + colecao_chave + "_" + algoritmo_chave + "_" + metrica_chave + ".csv", header=None, names = ['Qid', 'Valor'])
+                df['Coleção'] =  colecao_alias
+                df['Métrica'] = metrica_alias
+                df['Stemmer'] = algoritmo_alias
+                dfs.append(df)
+    
+    return pd.concat(dfs, axis = 0)
